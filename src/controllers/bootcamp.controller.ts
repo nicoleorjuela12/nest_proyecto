3da3555d9a5e6b26824d860fd033c8680b2bb72e
DarwinL14 <dslesmes6@soy.sena.edu.co>
2024-10-16 12:01:23 -0500

    metodos

--- src/controllers/bootcamp.controller.ts
+++ src/controllers/bootcamp.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post,Put } from '@nestjs/common';
 
 @Controller('bootcamp')
 export class BootcampController {
@@ -7,4 +7,24 @@ export class BootcampController {
     getAllBootcamps(): string{
         return "aqui se mostraran todos los bootcamps"
     }
+
+    @Get(":id")
+    getBootcampById (@Param('id') id: string):string{
+
+        return `aqui se va a mostrar el bootcamp con id ${id}` 	
+
+    }
+
+    @Post()
+    createBootcamp(): string{
+
+        return "aqui se creara un nuevo bootcamp";
+    }
+
+    @Put(":id")
+    updateBootcamp(@Param('id') id: string): string{
+        return `aqui se va a actualizar el bootcamp:  ${id}`;
+    }
+
+   
 }
